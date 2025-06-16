f16ff4f4acb0ae769ab7725194bd1b5a601580ec
Kseniia <kinozemtseva92@yandex.ru>
2025-06-16 22:13:05 +0300

    step 4

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  stylistic.configs.recommended,
+  pluginJs.configs.recommended,
+  {
+    files: ['*/.{js,ts,tsx}'],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+] // satisfies Linter.Config[]
