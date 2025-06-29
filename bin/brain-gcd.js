ab0e4efe3f436e1841e49d6b0386804729f131fd
Kseniia <kinozemtseva92@yandex.ru>
2025-06-29 13:54:51 +0300

    step 7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { runGame } from '../src/index.js'
+import { rules, generateRound } from '../src/games/gcd.js'
+
+runGame(rules, generateRound)
---
+++ src/games/gcd.js
@@ -0,0 +1,16 @@
+const getRandomNumber = (max = 100) => Math.floor(Math.random() * max) + 1
+
+const calculateGCD = (a, b) => {
+  if (!b) return a
+  return calculateGCD(b, a % b)
+}
+
+export const rules = 'Find the greatest common divisor of given numbers.'
+
+export const generateRound = () => {
+  const num1 = getRandomNumber()
+  const num2 = getRandomNumber()
+  const question = `${num1} ${num2}`
+  const answer = String(calculateGCD(num1, num2))
+  return [question, answer]
+}
