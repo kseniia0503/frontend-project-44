29875b7228033465379fdc649897063c272cffd3
Kseniia <kinozemtseva92@yandex.ru>
2025-06-29 15:02:50 +0300

    step 8

---
+++ bin/brain-progression.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { runGame } from '../src/index.js'
+import { rules, generateRound } from '../src/games/progression.js'
+
+runGame(rules, generateRound)
---
+++ src/games/progression.js
@@ -0,0 +1,21 @@
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+function generateProgression(start, step, length) {
+  return Array.from({ length }, (_, i) => start + i * step)
+}
+
+export const rules = 'What number is missing in the progression?'
+
+export const generateRound = () => {
+  const length = getRandomNumber(5, 10)
+  const start = getRandomNumber(1, 50)
+  const step = getRandomNumber(1, 10)
+  const hiddenIndex = getRandomNumber(0, length - 1)
+  const progression = generateProgression(start, step, length)
+  const answer = String(progression[hiddenIndex])
+
+  progression[hiddenIndex] = '..'
+  const question = progression.join(' ')
+
+  return [question, answer]
+}
